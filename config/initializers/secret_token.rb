4a09fc68ba209ef517c608805d90bab468e15179
rahulpatil_scs@yahoo.co.in <rahulpatil_scs@yahoo.co.in>
2013-05-02 00:40:03 +0530

    SecondCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaterPurifier::Application.config.secret_token = '3d0930d2dc42b29c1640f01839d87b48d9bec5c37a74c968922b686ec45220c7f1c22349b6921db553624654a4b4b26d08897a6fe3a1f993fd2eccb655154780'
